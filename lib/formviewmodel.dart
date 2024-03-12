4d3a4f88c7d90480132278f988201e2fcc08ab94
PratheeshBennet92 <55557123+PratheeshBennet92@users.noreply.github.com>
2024-03-12 23:16:32 +0400

    Dev spinner

--- lib/formviewmodel.dart
+++ lib/formviewmodel.dart
@@ -14,6 +14,8 @@ class FormData {
 
 // ViewModel class responsible for form logic and data manipulation
 class FormViewModel extends ChangeNotifier {
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
   final _formData = FormData();
 
   // Getters for accessing form data
@@ -40,9 +42,11 @@ class FormViewModel extends ChangeNotifier {
     final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
     return emailRegex.hasMatch(email);
   }
-  
+
   // Submit form method
   Future<void> submitForm() async {
+    _isLoading = true; // Set isLoading to true before making the API request
+    notifyListeners(); // Notify listeners immediately
     // Perform any additional logic here
     print('Form name ${_formData.name}');
     print('Foremailm designation ${_formData.designation}');
@@ -51,42 +55,48 @@ class FormViewModel extends ChangeNotifier {
     print('Form city ${_formData.city}');
     print('Form institution ${_formData.institution}');
 
+    Map<String, dynamic> participants = {
+      "participantName": "_formData.name",
+      "eventId": "30012024234548438",
+      "designation": "_formData.designation",
+      "contact": "_formData.contactNumber",
+      "city": "_formData.city",
+      "institution": "_formData.institution",
+      "participantId": "132",
+      "participantEmail": "pb33_db@example.com",
+      "status": "registered"
+      // Add other form fields as needed
+    };
 
-   Map<String, dynamic> participants = {
-  "participantName": "_formData.name",
-  "eventId": "30012024234548438",
-  "designation": "_formData.designation",
-  "contact": "_formData.contactNumber",
-  "city": "_formData.city",
-  "institution": "_formData.institution",
-  "participantId": "132",
-  "participantEmail": "pb33_db@example.com",
-  "status": "registered"
-  // Add other form fields as needed
-};
-
-Map<String, dynamic> requestBody = {
-  "parentDeviceId": "B287B760-8695-44E4-A4E9-4E55D8DC7A6",
-  "eventId": "12032024215641780",
-  "name": "Test Event 1",
-  "date": "25-07-2024",
-  "participants": [participants]
-};
-
-     print({requestBody});
+    Map<String, dynamic> requestBody = {
+      "parentDeviceId": "B287B760-8695-44E4-A4E9-4E55D8DC7A6",
+      "eventId": "12032024215641780",
+      "name": "Test Event 1",
+      "date": "25-07-2024",
+      "participants": [participants]
+    };
+    print({requestBody, _isLoading});
     // Make the API request
-    final url = Uri.parse('https://zjk4kiwx8h.execute-api.us-east-1.amazonaws.com/dev/item/');
+    final url = Uri.parse(
+        'https://zjk4kiwx8h.execute-api.us-east-1.amazonaws.com/dev/item/');
     final response = await http.put(
       url,
       body: json.encode(requestBody),
       headers: {'Content-Type': 'application/json'},
     );
-
+    // After the request is complete, set isLoading to false
+    _isLoading = false;
+    notifyListeners();
     // Check the response status
     if (response.statusCode == 200) {
       print('Form data submitted successfully');
     } else {
       print('Failed to submit form data: ${response.body}');
     }
+  }
+
+  void setLoading(bool bool) {
+    _isLoading = bool;
+    notifyListeners();
   }
 }
